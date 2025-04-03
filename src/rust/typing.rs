354acfe9a995240a8c4a0f657753bfff913d6b95
bem130 <bem130@ymail.ne.jp>
2025-04-03 10:18:37 +0900

    タイピングの終了判定

--- src/rust/typing.rs
+++ src/rust/typing.rs
@@ -47,40 +47,43 @@ pub fn key_input(mut model_: TypingModel,input: char) -> TypingModel {
     debug! {
         console_log!(format!("expect {:?}",&expect));
     }
+    let mut is_correct = false;
+    let mut is_finished = false;
     for e in expect {
         if e[model_.status.unconfirmed.len()] == input {
+            is_correct = true;
             // expectに一致
             if e.len() == model_.status.unconfirmed.len() + 1 {
                 // 完全一致
                 // 1文字進める
                 if remaining.len() == model_.status.char_ as usize + 1 {
                     if model_.content.lines[model_.status.line as usize].segments.len()==model_.status.segment as usize + 1 {
-                        // lineを進める
-                        model_.status.char_ = 0;
-                        model_.status.segment = 0;
-                        model_.status.line += 1;
-                        model_.status.unconfirmed.clear();
+                        if model_.content.lines.len() == model_.status.line as usize + 1 {
+                            // typing終了
+                            model_.status.char_ = 0;
+                            model_.status.segment = 0;
+                            model_.status.line = 0;
+                            model_.status.unconfirmed.clear();
+                            is_finished = true;
+                        } else {
+                            // lineを進める
+                            model_.status.char_ = 0;
+                            model_.status.segment = 0;
+                            model_.status.line += 1;
+                            model_.status.unconfirmed.clear();
+                        }
                     } else {
                         // segmentを進める
                         model_.status.char_ = 0;
                         model_.status.segment += 1;
                         model_.status.unconfirmed.clear();
                     }
-                    debug! {
-                        console_log!("correctA1", input);
-                    }
                 } else {
-                    debug! {
-                        console_log!("correctA2", input);
-                    }
                     // charを進める
                     model_.status.char_ += 1;
                     model_.status.unconfirmed.clear();
                 }
             } else {
-                debug! {
-                    console_log!("correctB", input);
-                }
                 // 一致したが、まだ続く
                 model_.status.unconfirmed.push(e[model_.status.unconfirmed.len()]);
             }
