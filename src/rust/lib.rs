72ea2298d82b811e093befd8faf224e3a16b5d38
bem130 <bem130@ymail.ne.jp>
2025-04-07 02:08:03 +0900

    web起動ちょっと変更

--- src/rust/lib.rs
+++ src/rust/lib.rs
@@ -7,7 +7,6 @@ use eframe::wasm_bindgen::{self, prelude::*};
 mod model;
 mod msg;
 mod update;
-mod jsapi;
 mod parser;
 mod typing;
 mod gui;
@@ -16,7 +15,7 @@ mod timestamp;
 
 
 #[wasm_bindgen]
-pub fn start_gui() -> Result<(), JsValue> {
+pub async  fn start_gui() -> Result<(), JsValue> {
     use wasm_bindgen::JsCast;
     use web_sys::HtmlCanvasElement;
     use crate::gui;
@@ -35,78 +34,76 @@ pub fn start_gui() -> Result<(), JsValue> {
         .unwrap();
 
     // Web (WASM) version
-    wasm_bindgen_futures::spawn_local(async move {
-        let web_options = eframe::WebOptions::default();
-        eframe::WebRunner::new()
-            .start(
-                canvas,
-                web_options,
-                Box::new(|cc| {
-                    // Configure font definitions
-                    let mut fonts = egui::FontDefinitions::default();
+    let web_options = eframe::WebOptions::default();
+    eframe::WebRunner::new()
+        .start(
+            canvas,
+            web_options,
+            Box::new(|cc| {
+                // Configure font definitions
+                let mut fonts = egui::FontDefinitions::default();
 
-                    fonts.font_data.insert(
-                        "KaiseiHarunoUmi".to_owned(),
-                        egui::FontData::from_static(include_bytes!("../fonts/KaiseiHarunoUmi-Bold.ttf")).into(),
-                    );
+                fonts.font_data.insert(
+                    "KaiseiHarunoUmi".to_owned(),
+                    egui::FontData::from_static(include_bytes!("../fonts/KaiseiHarunoUmi-Bold.ttf")).into(),
+                );
 
-                    fonts.font_data.insert(
-                        "Merienda".to_owned(),
-                        egui::FontData::from_static(include_bytes!("../fonts/Merienda-Regular.ttf")).into(),
-                    );
+                fonts.font_data.insert(
+                    "Merienda".to_owned(),
+                    egui::FontData::from_static(include_bytes!("../fonts/Merienda-Regular.ttf")).into(),
+                );
 
-                    fonts.font_data.insert(
-                        "YujiSyuku".to_owned(),
-                        egui::FontData::from_static(include_bytes!("../fonts/YujiSyuku-Regular.ttf")).into(),
-                    );
+                fonts.font_data.insert(
+                    "YujiSyuku".to_owned(),
+                    egui::FontData::from_static(include_bytes!("../fonts/YujiSyuku-Regular.ttf")).into(),
+                );
 
-                    fonts.font_data.insert(
-                        "ShipporiAntique".to_owned(),
-                        egui::FontData::from_static(include_bytes!("../fonts/ShipporiAntique-Regular.ttf")).into(),
-                    );
+                fonts.font_data.insert(
+                    "ShipporiAntique".to_owned(),
+                    egui::FontData::from_static(include_bytes!("../fonts/ShipporiAntique-Regular.ttf")).into(),
+                );
 
-                    fonts.font_data.insert(
-                        "NotoSerifJP".to_owned(),
-                        egui::FontData::from_static(include_bytes!("../fonts/NotoSerifJP-VariableFont_wght.ttf")).into(),
-                    );
+                fonts.font_data.insert(
+                    "NotoSerifJP".to_owned(),
+                    egui::FontData::from_static(include_bytes!("../fonts/NotoSerifJP-VariableFont_wght.ttf")).into(),
+                );
 
-                    // Configure the Proportional font family with YujiSyuku as primary and NotoSerifJP as fallback
-                    if let Some(proportional) = fonts.families.get_mut(&egui::FontFamily::Proportional) {
-                        proportional.clear();
-                        proportional.push("NotoSerifJP".to_owned());
-                    }
+                // Configure the Proportional font family with YujiSyuku as primary and NotoSerifJP as fallback
+                if let Some(proportional) = fonts.families.get_mut(&egui::FontFamily::Proportional) {
+                    proportional.clear();
+                    proportional.push("NotoSerifJP".to_owned());
+                }
 
-                    fonts.families.insert(
-                        egui::FontFamily::Name("main".into()),
-                        vec!["Merienda".to_owned(), "YujiSyuku".to_owned(), "ShipporiAntique".to_owned(), "NotoSerifJP".to_owned()],
-                    );
+                fonts.families.insert(
+                    egui::FontFamily::Name("main".into()),
+                    vec!["Merienda".to_owned(), "YujiSyuku".to_owned(), "ShipporiAntique".to_owned(), "NotoSerifJP".to_owned()],
+                );
 
-                    fonts.families.insert(
-                        egui::FontFamily::Name("kana".into()),
-                        vec!["KaiseiHarunoUmi".to_owned(), "ShipporiAntique".to_owned(), "YujiSyuku".to_owned()],
-                    );
+                fonts.families.insert(
+                    egui::FontFamily::Name("kana".into()),
+                    vec!["KaiseiHarunoUmi".to_owned(), "ShipporiAntique".to_owned(), "YujiSyuku".to_owned()],
+                );
 
-                    fonts.families.insert(
-                        egui::FontFamily::Name("ruby".into()),
-                        vec!["Merienda".to_owned(), "ShipporiAntique".to_owned(), "YujiSyuku".to_owned(), "KaiseiHarunoUmi".to_owned(), "NotoSerifJP".to_owned()],
-                    );
+                fonts.families.insert(
+                    egui::FontFamily::Name("ruby".into()),
+                    vec!["Merienda".to_owned(), "ShipporiAntique".to_owned(), "YujiSyuku".to_owned(), "KaiseiHarunoUmi".to_owned(), "NotoSerifJP".to_owned()],
+                );
 
-                    // Apply the customized fonts to the egui context
-                    cc.egui_ctx.set_fonts(fonts);
+                // Apply the customized fonts to the egui context
+                cc.egui_ctx.set_fonts(fonts);
 
-                    // Set the default theme to dark mode
-                    let style = egui::Style {
-                        visuals: egui::Visuals::dark(),
-                        ..egui::Style::default()
-                    };
-                    cc.egui_ctx.set_style(style);
+                // Set the default theme to dark mode
+                let style = egui::Style {
+                    visuals: egui::Visuals::dark(),
+                    ..egui::Style::default()
+                };
+                cc.egui_ctx.set_style(style);
 
-                    Ok(Box::new(gui::TypingApp::default()))
-                }),
-            )
-            .await
-            .expect("failed to start eframe");
-    });
+                Ok(Box::new(gui::TypingApp::default()))
+            }),
+        )
+        .await
+        .expect("failed to start eframe");
 
     Ok(())
 }
--- src/web/index.ts
+++ src/web/index.ts
@@ -2,7 +2,7 @@ import { elm, textelm } from './cdom.js';
 import { initlayout } from "./layout.js";
 import { Model, Msg, Segment, TextOrientation, TypingCorrectnessSegment, TypingMetrics, TypingStatus } from "./model.js";
 
-import initWasm, { start_gui ,init_model, event_receive_keyboard, fetch_render_data, add_contents, typing_scroll } from './typing_lib.js';
+import initWasm, { start_gui } from './typing_lib.js';
 
 
 async function run() {
